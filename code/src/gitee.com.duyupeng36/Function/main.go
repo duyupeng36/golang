86e378c8bd94bd6abfe0bfec990c3eb614d4ac3a
杜宇鹏 <2321936402@qq.com>
2021-03-30 10:38:46 +0800

    匿名函数与递归函数

--- code/src/gitee.com.duyupeng36/Function/main.go
+++ code/src/gitee.com.duyupeng36/Function/main.go
@@ -51,6 +51,70 @@ func calc(x, y int) (sum, sub int) {
 
 type funcType func(int, int) (int, int)
 
+func plus(a ...int) int {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	return sum
+}
+
+func minus(a ...int) int {
+	return a[0] - a[1]
+}
+
+func calculator(option func(...int) int, a ...int) (int, func(...int) int) {
+	return option(a...), option
+}
+
+func function1(a int, b int) int {
+
+	f := func(x int, y int) int {
+		return x + y
+	} // 匿名函数
+
+	return f(a, b)
+}
+
+func function2(a int, b int) int {
+
+	f := func(x int, y int) int {
+		return x + y
+	}(a, b) // 定义匿名函数，立即执行
+	return f
+}
+
+// 该函数就是一个闭包函数
+func adder() func(int) int {
+	var x int
+	return func(y int) int {
+		x += y
+		return x
+	}
+}
+
+func factorial(n int64) (result int64) {
+	if n < 1 {
+		result = 1
+	} else {
+		result = n * factorial(n-1)
+	}
+	return
+}
+
+var count [1000]int64
+
+func fibolach(x int) int64 {
+	if x == 1 || x == 2 {
+		return 1
+	} else if count[x-1] != 0 && count[x-2] != 0 {
+		return count[x-1] + count[x-2]
+	} else {
+		count[x-1] = fibolach(x - 1)
+		count[x-2] = fibolach(x - 2)
+		return count[x-1] + count[x-2]
+	}
+}
 func main() {
 	//z := add(10, 20) // 调用函数
 	//fmt.Println(z)
@@ -81,15 +145,34 @@ func main() {
 	//x, y = 30, -10
 	//x, y = ca(x, y)
 	//fmt.Println(x, y)
+	//
+	//fmt.Println(a)
+	//a = 20
+	//fmt.Println(a)
+	//
+	////fmt.Println(b)  // 无法访问变量b
+	//b := 30
+	//fmt.Println(b) // 变量定义之后才能访问
 
-	fmt.Println(a)
-	a = 20
-	fmt.Println(a)
+	//result, option := calculator(plus, 1,2,3,4)
+	//fmt.Println(result, option)
+	//result, option = calculator(minus, 5, 4)
+	//fmt.Println(result, option)
+	//
+	//fmt.Println(function1(10, 20))
+	//
+	//fmt.Println(function2(30, 20))
+	//{
+	//	fmt.Println("hello 匿名函数1")
+	//}
+	//
+	//func () {
+	//	fmt.Println("hello 匿名函数2")
+	//}()
 
-	//fmt.Println(b)  // 无法访问变量b
-	b := 30
-	fmt.Println(b) // 变量定义之后才能访问
-
+	fmt.Println(factorial(13))
+	fibolach(10)
+	fmt.Println(count[0:10])
 }
 
-var a int = 10
+//var a int = 10
