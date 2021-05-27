9314425ee318c554c379bd6439a5ce1b2ca3db89
静谧之裳 <2321936402@qq.com>
2021-05-27 22:03:39 +0800

    context使用

---
+++ code/Context/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func f(ctx context.Context) {
+	defer wg.Done()
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		i := 0
+		for {
+			select {
+			case <-ctx.Done():
+				runtime.Goexit() // 退出当前goroutine
+			default: // 如果没有default该子goroutine将阻塞
+			}
+			i++
+			fmt.Printf("---2执行时间%v\n", i)
+			time.Sleep(time.Second)
+		}
+	}(ctx)
+
+	i := 0
+	for {
+		select {
+		case <-ctx.Done():
+			runtime.Goexit() // 退出当前goroutine
+		default: // 如果没有default该子goroutine将阻塞
+		}
+		i++
+		fmt.Printf("1执行时间%v\n", i)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	//d := time.Now().Add(time.Second * 5)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5) // 返回一个ctx对象和取消函数
+	defer cancel()
+
+	wg.Add(1)
+	go f(ctx)
+	//<-time.After(time.Second * 5)  // 等待3秒
+	//cancel()  // 通知子goroutine关闭
+	wg.Wait()
+}
---
+++ code/Redis/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/go-redis/redis"
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// 初始化连接
+func initClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "121.5.72.146:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err = rdb.Ping().Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+
+
+
+}
+
